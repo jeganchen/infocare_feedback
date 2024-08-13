87a5cae09932f315d5e25c883b3122eeb9ed80dd
Jegan <jzgchen@163.com>
2024-08-13 11:29:10 +0800

    only allow admin view unassigned folder

--- resources/views/mailboxes/partials/folders.blade.php
+++ resources/views/mailboxes/partials/folders.blade.php
@@ -16,18 +16,20 @@
             } else {
                 $active_count = $folder_item->getCount($folders);
             }
-        @endphp
-        <li class="{{ $folder_item->getTypeIcon() }}@if ($folder_item->id == $folder->id) active @endif" data-folder_id="{{ $folder_item->id }}" data-active-count="{{ $folder_item->active_count }}">
-            <a href="{{ $folder_item->url($mailbox->id) }}" @if (!$folder_item->active_count) class="no-active" @endif><i class="glyphicon glyphicon-{{ $folder_item->getTypeIcon() }}"></i> <span class="folder-name">{{ $folder_item->getTypeName() }}</span>
-                
-                @if ($active_count)
-                    @if ($folder_item->type == App\Folder::TYPE_UNASSIGNED || $folder_item->type == App\Folder::TYPE_MINE)
-                        <strong class="active-count pull-right" data-toggle="tooltip" title="{{ __("Active Conversations") }}">{{ $active_count }}</strong>
-                    @else
-                        <span class="active-count pull-right" data-toggle="tooltip" title="{{ __("Active Conversations") }}">{{ $active_count }}</span>
+        @endphp 
+        @if ($user->isAdmin() && $folder_item->type == App\Folder::TYPE_UNASSIGNED || $folder_item->type != App\Folder::TYPE_UNASSIGNED)
+            <li class="{{ $folder_item->getTypeIcon() }}@if ($folder_item->id == $folder->id) active @endif" data-folder_id="{{ $folder_item->id }}" data-active-count="{{ $folder_item->active_count }}">
+                <a href="{{ $folder_item->url($mailbox->id) }}" @if (!$folder_item->active_count) class="no-active" @endif><i class="glyphicon glyphicon-{{ $folder_item->getTypeIcon() }}"></i> <span class="folder-name">{{ $folder_item->getTypeName() }}</span>
+                    
+                    @if ($active_count)
+                        @if ($folder_item->type == App\Folder::TYPE_UNASSIGNED || $folder_item->type == App\Folder::TYPE_MINE)
+                            <strong class="active-count pull-right" data-toggle="tooltip" title="{{ __("Active Conversations") }}">{{ $active_count }}</strong>
+                        @else
+                            <span class="active-count pull-right" data-toggle="tooltip" title="{{ __("Active Conversations") }}">{{ $active_count }}</span>
+                        @endif
                     @endif
-                @endif
-            </a>
-        </li>
+                </a>
+            </li>
+        @endif
     @endif
 @endforeach
