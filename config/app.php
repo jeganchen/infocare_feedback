5093051bd365ecfe54be26f35aa207a405ba62d1
Jegan <jzgchen@163.com>
2024-08-13 14:01:49 +0800

    support en and zh-cn only

--- config/app.php
+++ config/app.php
@@ -98,8 +98,9 @@ return [
     */
 
     'locale'          => env('APP_LOCALE', 'en'),
-    'locales'         => ['en', 'zh-CN', 'hr', 'cs', 'da', 'nl', 'fi', 'fr', 'de', 'it', 'ja', 'ko', 'no', 'fa', 'pl', 'pt-PT', 'pt-BR', 'ru', 'es', 'sk', 'sv', 'tr'],
-    'locales_rtl'     => ['fa'],
+    'locales'         => ['en', 'zh-CN'],
+    // 'locales'         => ['en', 'zh-CN', 'hr', 'cs', 'da', 'nl', 'fi', 'fr', 'de', 'it', 'ja', 'ko', 'no', 'fa', 'pl', 'pt-PT', 'pt-BR', 'ru', 'es', 'sk', 'sv', 'tr'],
+    'locales_rtl'     => ['en'],
     'default_locale'  => 'en',
 
     /*
